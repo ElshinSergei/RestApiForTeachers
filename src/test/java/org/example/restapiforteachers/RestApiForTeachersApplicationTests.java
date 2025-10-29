f2ed4e73b05e6abfded23ad0d8517082fa8fac04
Elshin Sergei <elshin.sergei@list.ru>
2025-10-30 02:15:13 +0500

    finished

---
+++ src/test/java/org/example/restapiforteachers/RestApiForTeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restapiforteachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForTeachersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
